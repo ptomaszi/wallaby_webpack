152d3ae941daa3aaa2c72a4e7486a54356bd4b95
Artem Govorov <artem.govorov@gmail.com>
2017-09-15 19:44:32 +1000

    Global objects example

---
+++ src/di.js
@@ -0,0 +1,12 @@
+define(function () {
+  window.$di = window.$di || {};
+  window.$di.angular = window.$di.angular || {};
+  window.$di.debug = window.$di.debug || {};
+  window.$di.event = window.$di.event || {};
+  window.$di.exception = window.$di.exception || {};
+  window.$di.utility = window.$di.utility || {};
+  window.$di.utility.placeholders = window.$di.utility.placeholders || {};
+  window.$di.utility.placeholders.arguments = window.$di.utility.placeholders.arguments || {};
+
+  return;
+});
